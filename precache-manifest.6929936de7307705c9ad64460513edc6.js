abc7636bc36aab36a72c845b36612fbeeb541e60
Mitchell Kehn <mitchellkehn@gmail.com>
2020-06-08 21:38:49 +1000

    Updates

---
+++ precache-manifest.6929936de7307705c9ad64460513edc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39de670b5826af68a0c330c4ba0c0ed7",
+    "url": "/boogle/index.html"
+  },
+  {
+    "revision": "9eba4dfef20733a5e2a7",
+    "url": "/boogle/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "543fa4822c3465b85543",
+    "url": "/boogle/static/js/2.51de7286.chunk.js"
+  },
+  {
+    "revision": "a1d2ec531816eb43d39a402dcaccf569",
+    "url": "/boogle/static/js/2.51de7286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eba4dfef20733a5e2a7",
+    "url": "/boogle/static/js/main.2e6cabff.chunk.js"
+  },
+  {
+    "revision": "c68ae94b0b37fdf0c9ff",
+    "url": "/boogle/static/js/runtime-main.89010e31.js"
+  }
+]);
